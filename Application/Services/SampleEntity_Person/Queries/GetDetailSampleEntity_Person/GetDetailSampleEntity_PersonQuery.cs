daa693c3b5e287fb5f6391923c00e84042c8b36f
Saba Babaei <sabababaei1393@gmail.com>
2022-12-30 02:54:36 +0330

    create application layer folders for sample entity

---
+++ Application/Services/SampleEntity_Person/Queries/GetDetailSampleEntity_Person/GetDetailSampleEntity_PersonQuery.cs
@@ -0,0 +1,6 @@
+namespace Application.Services.SampleEntity_Person.Queries.GetDetailSampleEntity_Person;
+
+public class GetDetailSampleEntity_PersonQuery
+{
+    
+}
---
+++ Application/Services/SampleEntity_Person/Queries/GetListSampleEntity_Person/GetListSampleEntity_PersonQueryViewModel.cs
@@ -0,0 +1,6 @@
+namespace Application.Services.SampleEntity_Person.Queries.GetListSampleEntity_Person;
+
+public class GetListSampleEntity_PersonQueryViewModel
+{
+    
+}
