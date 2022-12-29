103b8535ac0c9d2a49adca9f83b5454b6cc30593
Saba Babaei <sabababaei1393@gmail.com>
2022-12-30 02:54:40 +0330

    created validators

--- Application/Services/SampleEntity_Person/Commands/UpdateSampleEntity_Person/UpdateSampleEntity_PersonCommandValidator.cs
+++ Application/Services/SampleEntity_Person/Commands/UpdateSampleEntity_Person/UpdateSampleEntity_PersonCommandValidator.cs
@@ -1,6 +1,8 @@
+using FluentValidation;
+
 namespace Application.Services.SampleEntity_Person.Commands.UpdateSampleEntity_Person;
 
-public class CreateSampleEntity_PersonCommandValidator
+public class UpdateSampleEntity_PersonCommandValidator:AbstractValidator<UpdateSampleEntity_PersonCommand>
 {
     
 }
