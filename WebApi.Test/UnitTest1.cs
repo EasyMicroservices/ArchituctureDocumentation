2d12a7df32e5d6f4a664f32a608e114a469f32e0
Saba Babaei <sabababaei1393@gmail.com>
2022-12-30 02:54:33 +0330

    initial

---
+++ WebApi.Test/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace WebApi.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ WebSite.Test/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace WebSite.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
